7f23865e708e6b1654984adcb8ba5c7540abb2d1
edenszetohk01 <31842071+edenszetohk01@users.noreply.github.com>
2017-11-09 11:38:37 +0800

    only inject base url to assets path on production build (#11)

--- gulp/tasks/inject.js
+++ gulp/tasks/inject.js
@@ -1,18 +1,34 @@
 'use strict';
+const argv = require('yargs').argv;
 const gulp = require('gulp');
 const inject = require('gulp-inject');
+const when = require('gulp-if');
 const pkg = require('../../package');
 
 // 'gulp inject:head' -- injects our style.css file into the head of our HTML
 gulp.task('inject:head', () =>
   gulp.src('.tmp/src/_includes/head.html')
-    .pipe(inject(gulp.src('.tmp/assets/stylesheets/*.css'), {ignorePath: '.tmp', addPrefix: pkg.homepage, addRootSlash: false}))
+    .pipe(when(argv.prod, inject(gulp.src('.tmp/assets/stylesheets/*.css'), {
+      ignorePath: '.tmp',
+      addPrefix: pkg.homepage,
+      addRootSlash: false
+    })))
+    .pipe(when(!argv.prod, inject(gulp.src('.tmp/assets/stylesheets/*.css'), {
+      ignorePath: '.tmp'
+    })))
     .pipe(gulp.dest('.tmp/src/_includes'))
 );
 
 // 'gulp inject:footer' -- injects our index.js file into the end of our HTML
 gulp.task('inject:footer', () =>
   gulp.src('.tmp/src/_layouts/default.html')
-    .pipe(inject(gulp.src('.tmp/assets/javascript/*.js'), {ignorePath: '.tmp', addPrefix: pkg.homepage, addRootSlash: false}))
+    .pipe(when(argv.prod, inject(gulp.src('.tmp/assets/javascript/*.js'), {
+      ignorePath: '.tmp',
+      addPrefix: pkg.homepage,
+      addRootSlash: false
+    })))
+    .pipe(when(!argv.prod, inject(gulp.src('.tmp/assets/javascript/*.js'), {
+      ignorePath: '.tmp'
+    })))
     .pipe(gulp.dest('.tmp/src/_layouts'))
 );
